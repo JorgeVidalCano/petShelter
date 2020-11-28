f81351671dbaea227f0e11403faa71132f2d9f8d
JorgeVidalCano <jorge.vidal.cano@gmail.com>
2020-11-28 11:07:33 +0100

    deploy

--- petShelter/settings.py
+++ petShelter/settings.py
@@ -1,4 +1,4 @@
-# import django_heroku
+import django_heroku
 from pathlib import Path
 import os
 import ast
@@ -156,4 +156,4 @@ AWS_S3_REGION_NAME='eu-west-3'
 
 DEFAULT_FILE_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
